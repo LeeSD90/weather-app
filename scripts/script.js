b0328fe5f8a17964e0fc031d67b96a7bb299bbf4
LeeSD90 <lee.doyle90@gmail.com>
2018-07-25 11:26:22 +0200

    refactor and extra styling

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,20 @@
 document.addEventListener("DOMContentLoaded", function(e) {
-  document.getElementById("get-weather").addEventListener("click", () => fetchWeather(document.getElementById("location").value));
+  document.getElementById("get-weather").addEventListener("click", updateWeatherDisplay);
 });
+
+function updateWeatherDisplay() {
+  const data = fetchWeather(document.getElementById("location").value);
+  data.then(r => {
+    document.getElementById("result").innerHTML = "Temperature: " + r.main.temp + " &deg;C";
+  });
+}
 
 async function fetchWeather(location) {
   try {
     const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=metric&APPID=" + APPID, {mode: 'cors'});
     const data = await response.json();
-    console.log(data);
     if(data.cod == "200") {
-      document.getElementById("result").innerHTML = data.main.temp + " &deg;C";
-    }
-    else {
-      this.error = "Woops, error!";
-      this.shown = false;
+      return data;
     }
   }
   catch(e) {
@@ -21,3 +23,4 @@ async function fetchWeather(location) {
 }
 
 const APPID = "e25872c6e80b8a83e7ba15ec135fcbfc";
+
